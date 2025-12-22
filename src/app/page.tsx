761260031c394ad98518611198d633cb1ce8f692
vipinAlways <vipinkumarti555@gmail.com>
2025-12-22 17:13:19 +0530

    last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
 import { ArrowRight, Check, Star } from "lucide-react";
 import Phone from "./components/Phone";
@@ -7,7 +7,7 @@ import { Reviews } from "./components/Reviews";
 import Link from "next/link";
 import { buttonVariants } from "./components/ui/button";
 import { Icons } from "./components/Icon";
-import Image from "next/image";
+
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <Image fill src="/snake-1.png" className="w-full" alt="snake" />
+                <img src="/snake-1.png" className="w-full" alt="snake" />
               </div>
               <h1 className="relative w-fit tracking-tighter text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl ">
                 Your Image on a{" "}
@@ -47,32 +47,27 @@ const Home = () => {
                 </div>
               </ul>
               <div className="mb-4 mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
-                <Image
-                  fill
+                <img
                   src="/users/user-1.png"
                   className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   alt="user Image"
                 />
-                <Image
-                  fill
+                <img
                   src="/users/user-2.png"
                   className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   alt="user Image"
                 />
-                <Image
-                  fill
+                <img
                   src="/users/user-3.png"
                   className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   alt="user Image"
                 />
-                <Image
-                  fill
+                <img
                   src="/users/user-4.jpg"
                   className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   alt="user Image"
                 />
-                <Image
-                  fill
+                <img
                   src="/users/user-5.jpg"
                   className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover"
                   alt="user Image"
@@ -95,14 +90,12 @@ const Home = () => {
 
           <div className="col-span-full lg:col-span-1 w-full justify-center px-8 sm:px-16 md:px-0 mt-31 lg:mt-20 lg:mx-0 h-fit">
             <div className="relative md:max-w-xl">
-              <Image
-                fill
+              <img
                 src="/your-image.png"
                 alt=""
                 className="absolute min-w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
               />
-              <Image
-                fill
+              <img
                 src="/line.png"
                 alt=""
                 className="absolute w-20 -left-6 -bottom-6 select-none "
@@ -122,8 +115,7 @@ const Home = () => {
               </span>{" "}
               say
             </h2>
-            <Image
-              fill
+            <img
               src="/snake-2.png"
               className="w-24 order-0 lg:order-2 "
               alt=""
@@ -151,8 +143,7 @@ const Home = () => {
                 </p>
               </div>
               <div className="flex gap-2 mt-2">
-                <Image
-                  fill
+                <img
                   src="/users/user-1.png"
                   className="rounded-full h-12 w-12 object-cover"
                   alt="user"
@@ -187,8 +178,7 @@ const Home = () => {
                 </p>
               </div>
               <div className="flex gap-2 mt-2">
-                <Image
-                  fill
+                <img
                   src="/users/user-3.png"
                   className="rounded-full h-12 w-12 object-cover"
                   alt="user"
@@ -223,15 +213,13 @@ const Home = () => {
           </div>
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
-              <Image
-                fill
+              <img
                 src="/arrow.png"
                 className="absolute top-[25rem] md:top-1/2 lg:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
                 alt="arrow"
               />
               <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl ">
-                <Image
-                  fill
+                <img
                   src="/horse.jpg"
                   alt="horse"
                   className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full "
